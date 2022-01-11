a3b3d76a6d313b5a2a81695b37af3dbcc66e4046
frankhillard <frank.hillard@smart-chain.fr>
2022-01-11 12:57:14 +0100

    jsligo implem + pytezos test + ligo test

---
+++ advisor/views_hangzhou/cameligo/test/ligo/test.mligo
@@ -0,0 +1,51 @@
+#include "../../advisor.mligo"
+#include "../../indice.mligo"
+
+let test =
+  
+  // deploy INDICE contract 
+  let indice_initial_storage = 4 in
+  let iis = Test.run (fun (x:indiceStorage) -> x) indice_initial_storage in
+  let indice_contract_path = "views_hangzhou/cameligo/indice.mligo" in
+  let (address_indice, code_indice, _) = Test.originate_from_file indice_contract_path "indiceMain" (["indice_value"] : string list) iis 0tez in
+  let actual_storage = Test.get_storage_of_address address_indice in
+  let indice_taddress = (Test.cast_address address_indice : (indiceEntrypoints,indiceStorage) typed_address) in
+  let indice_contract = Test.to_contract indice_taddress in
+
+  // INDICE Increment(1)
+  let () = Test.transfer_to_contract_exn indice_contract (Increment(1)) 0mutez in
+  let inc_actual_storage = Test.get_storage indice_taddress in
+  let () = Test.log(inc_actual_storage) in
+  let () = assert(inc_actual_storage = indice_initial_storage + 1) in
+
+  // INDICE Decrement(2)
+  let () = Test.transfer_to_contract_exn indice_contract (Decrement(2)) 0mutez in
+  let dec_actual_storage = Test.get_storage indice_taddress in
+  let () = Test.log(dec_actual_storage) in
+  let () = assert(dec_actual_storage = inc_actual_storage - 2) in
+
+  // deploy ADVISOR contract 
+  let advisor_initial_storage : advisorStorage = {indiceAddress=address_indice; algorithm=(fun(i : int) -> if i < 10 then True else False); result=False} in
+  let ais = Test.run (fun (x:advisorStorage) -> x) advisor_initial_storage in
+  let advisor_contract_path = "views_hangzhou/cameligo/advisor.mligo" in
+  let (address_advisor, code_advisor, _) = Test.originate_from_file advisor_contract_path "advisorMain" ([] : string list) ais 0tez in
+  let advisor_taddress = (Test.cast_address address_advisor : (advisorEntrypoints,advisorStorage) typed_address) in
+  let advisor_contract = Test.to_contract advisor_taddress in
+
+  // ADVISOR call ExecuteAlgorithm
+  let () = Test.transfer_to_contract_exn advisor_contract (ExecuteAlgorithm(unit)) 0mutez in
+  let advisor_modified_storage = Test.get_storage advisor_taddress in
+  let () = Test.log(advisor_modified_storage) in
+  let () = assert(advisor_modified_storage.result = True) in
+
+  // ADVISOR call ChangeAlgorithm
+  let new_algo : int -> bool = (fun(i : int) -> if i < 3 then True else False) in
+  let () = Test.transfer_to_contract_exn advisor_contract (ChangeAlgorithm(new_algo)) 0mutez in
+  let advisor_modified_storage2 = Test.get_storage advisor_taddress in
+  //let () = Test.log(advisor_modified_storage2.algorithm) in
+
+  // ADVISOR call ExecuteAlgorithm
+  let () = Test.transfer_to_contract_exn advisor_contract (ExecuteAlgorithm(unit)) 0mutez in
+  let advisor_modified_storage3 = Test.get_storage advisor_taddress in
+  let () = Test.log(advisor_modified_storage3) in
+  assert(advisor_modified_storage3.result = False)
---
+++ advisor/views_hangzhou/cameligo/test/pytezos/test_advisor.py
@@ -0,0 +1,87 @@
+from unittest import TestCase
+from contextlib import contextmanager
+from copy import deepcopy
+from pytezos import ContractInterface, MichelsonRuntimeError, pytezos
+from pytezos.michelson.types.big_map import big_map_diff_to_lazy_diff
+from pytezos.michelson.types.option import OptionType
+from pytezos.michelson.types.domain import LambdaType
+from pytezos.michelson.types.option import SomeLiteral, NoneLiteral
+from pytezos import michelson_to_micheline
+from typing import Optional
+import time
+import json 
+
+alice = 'tz1hNVs94TTjZh6BZ1PM5HL83A7aiZXkQ8ur'
+admin = 'tz1fABJ97CJMSP2DKrQx2HAFazh6GgahQ7ZK'
+bob = 'tz1c6PPijJnZYjKiSQND4pMtGMg6csGeAiiF'
+# oscar = 'tz1Phy92c2n817D17dUGzxNgw1qCkNSTWZY2'
+# fox = 'tz1XH5UyhRCUmCdUUbqD4tZaaqRTgGaFXt7q'
+
+indiceAddress = "KT1BEqzn5Wx8uJrZNvuS9DVHmLvG9td3fDLi" # Hardcoded farm address for tests
+compiled_contract_path = "../../compiled/advisor.tz"
+initial_lambda = "{ PUSH int 10 ; SWAP ; COMPARE ; LT ; IF { PUSH bool True } { PUSH bool False } }"
+initial_storage = ContractInterface.from_file(compiled_contract_path).storage.dummy()
+initial_storage["indiceAddress"] = indiceAddress
+initial_storage["algorithm"] = initial_lambda # LambdaType.from_micheline_value(michelson_to_micheline(initial_lambda))
+initial_storage["result"] = False
+# initial_result : Optional[bool] = None
+# initial_storage["result"] = initial_result
+print(initial_storage["algorithm"])
+missing_entrypoint_sendvalue  = "the targeted contract has not entrypoint sendValue"
+
+
+class AdvisorContractTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.advisor = ContractInterface.from_file(compiled_contract_path)
+        cls.maxDiff = None
+
+    @contextmanager
+    def raisesMichelsonError(self, error_message):
+        with self.assertRaises(MichelsonRuntimeError) as r:
+            yield r
+
+        error_msg = r.exception.format_stdout()
+        if "FAILWITH" in error_msg:
+            self.assertEqual(f"FAILWITH: '{error_message}'", r.exception.format_stdout())
+        else:
+            self.assertEqual(f"'{error_message}': ", r.exception.format_stdout())
+
+    #############################
+    # Tests for changeAlgorithm #
+    #############################
+
+    def test_change_algorithm_should_work(self):
+        # Init
+        init_storage = deepcopy(initial_storage)
+        # Execute entrypoint
+        new_lambda = "{ PUSH int 10 ; SWAP ; COMPARE ; LT ; IF { PUSH bool True } { PUSH bool False } }"
+        # new_algo_param = michelson_to_micheline(new_lambda)
+        # new_lambda_string = "{ PUSH int 10 ; SWAP ; COMPARE ; LT ; IF { PUSH bool True } { PUSH bool False } }"
+        res = self.advisor.changeAlgorithm(new_lambda).interpret(storage=init_storage, sender=admin)
+        self.assertEqual(new_lambda, res.storage["algorithm"])
+        self.assertEqual([], res.operations)
+
+    def test_receive_value_should_work(self):
+        # Init
+        init_storage = deepcopy(initial_storage)
+        # Execute entrypoint
+        res = self.advisor.receiveValue(9).interpret(storage=init_storage, sender=admin)
+        self.assertEqual(res.storage["result"], True)
+        self.assertEqual([], res.operations)
+
+    def test_request_value_should_work(self):
+        # Init
+        init_storage = deepcopy(initial_storage)
+        # Execute entrypoint
+        res = self.advisor.requestValue().interpret(storage=init_storage, sender=admin)
+        self.assertEqual(res.storage["result"], False)
+        self.assertEqual(res.storage["algorithm"], initial_storage["algorithm"])
+        self.assertEqual(len(res.operations), 1)
+
+    def test_request_value_should_fail(self):
+        # Init
+        init_storage = deepcopy(initial_storage)
+        # Execute entrypoint
+        with self.raisesMichelsonError(missing_entrypoint_sendvalue):
+            self.advisor.requestValue().interpret(storage=init_storage, sender=alice)
