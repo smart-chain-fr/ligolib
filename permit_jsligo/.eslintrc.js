d73625f1f6da6a8cf4bfdf43f974b76f987b6a5a
frankhillard <frank.hillard@smart-chain.fr>
2022-08-29 13:59:00 +0200

    remove old permit folder

--- permit_jsligo/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  plugins: ["@typescript-eslint"],
-};
